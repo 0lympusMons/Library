3947fbf88abd50a0c34b1c2ebed658611ccf5b9f
0lympusMons <dodgeynohangelomadula2002@gmail.com>
2023-07-01 03:29:54 +0800

    Add changes to script

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
+let addBookButton = document.querySelector(".add-book-button");
+
+addBookButton.addEventListener('click', () => {
+    let authorName = "Dodot";
+
+    let bookCard = document.createElement('div');
+    
+    let bookCardContent = `<div class="book__card--title">Dune</div>
+    <div class="book__card--author">${authorName}</div>
+    <div class="book__card--pages">400 pages</div>
+    <div class="book__card--read_status"><button>Read</button></div>
+    <div class="book__card--remove"><button>Remove</button></div>`;
+    
+    bookCard.classList.add('book__card');
+    bookCard.innerHTML = bookCardContent;
+    document.querySelector(".books__wrapper").appendChild(bookCard);
+
+});
+
+
+
+
 let myLibrary = [];
 
 function Book() {
-  // the constructor...
+    // the constructor...
 }
 
 function addBookToLibrary() {
-  // do stuff here
+    // do stuff here
 }
+
